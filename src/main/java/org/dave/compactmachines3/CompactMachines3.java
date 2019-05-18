f7a54ef8a7c895bda764fcd66cab543cc1c8cb5e
= <ShinyTourettesX@gmail.com>
2019-05-18 12:35:11 +0800

    Added a flag as suggested by julian for compatability when running mismatched jars with serverside changes.

--- src/main/java/org/dave/compactmachines3/CompactMachines3.java
+++ src/main/java/org/dave/compactmachines3/CompactMachines3.java
@@ -29,7 +29,7 @@ import org.dave.compactmachines3.world.WorldSavedDataMachines;
 import org.dave.compactmachines3.world.data.provider.ExtraTileDataProviderRegistry;
 import org.dave.compactmachines3.world.tools.DimensionTools;
 
-@Mod(modid = CompactMachines3.MODID, version = CompactMachines3.VERSION, acceptedMinecraftVersions = "[1.12,1.13)", dependencies = "after:refinedstorage;after:yunomakegoodmap", guiFactory = CompactMachines3.GUI_FACTORY)
+@Mod(modid = CompactMachines3.MODID, version = CompactMachines3.VERSION, acceptedMinecraftVersions = "[1.12,1.13)", dependencies = "after:refinedstorage;after:yunomakegoodmap", guiFactory = CompactMachines3.GUI_FACTORY, acceptableRemoteVersions = "*")
 public class CompactMachines3
 {
     public static final String MODID = "compactmachines3";
